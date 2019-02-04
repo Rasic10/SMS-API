1402414b659ad90a6138b6bf3154470ffce80904
Nemanja Rasic <nemanja.rasic10@gmail.com>
2019-02-04 23:47:53 +0100

    Create clientChat.py

---
+++ clientChat.py
@@ -0,0 +1,36 @@
+from socket import *
+from time import sleep
+from threading import *
+
+class ListenThread(Thread):
+    def __init__(self, socket):
+        self.sock = socket
+        super().__init__()
+        self.start()
+
+    def run(self):
+        print('Now listening!')
+        while True:
+            print(self.sock.recv(4096).decode())
+
+srv_address = 'localhost'
+srv_port = 11111
+
+username = input('Enter your username: ')
+
+while True:
+    try:
+        socket = socket(AF_INET, SOCK_STREAM)
+        socket.connect((srv_address, srv_port))
+        print('Connected to {} on port {}'.format(srv_address, srv_port))
+        socket.send(username.encode())
+        listener = ListenThread(socket)
+        break
+    except:
+        print('Unable to connect to the server')
+        print('Retrying in 5 seconds...')
+        sleep(5)
+
+while True:
+    message = input()
+    socket.send(message.encode())
